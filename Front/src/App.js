0fd21f227cffc0c9db0a04dd0c8d3c38e87270b9
marwaktataa <marwaktata15@gmail.com>
2019-11-28 22:30:12 +0100

    test groupName if not exist

---
+++ Front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulaire from './components/baby'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Formulaire/>
+    </div>
+  );
+}
+
+export default App;
