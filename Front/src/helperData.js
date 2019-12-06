9bb6bc9c523b711334eae241cd69c40a71202c10
jobran41 <jobran41@gmail.com>
2019-12-06 22:01:48 +0100

    add data helper

---
+++ Front/src/helperData.js
@@ -0,0 +1,24 @@
+export const parents=[
+    {email:'a@gm.com',id:1,age:30,sexe:'home',firstName:'mahmoud',lastName:'hajri',tel:56990345,child:[330,331,332]},
+    {email:'a@gm.com',id:2,age:30,sexe:'home',firstName:'salah',lastName:'fahmi',tel:56955345,child:[333,334]},
+    {email:'a@gm.com',id:3,age:30,sexe:'home',firstName:'fathi',lastName:'amairi',tel:56990345,child:[335,336,337]},
+    {email:'a@gm.com',id:4,age:30,sexe:'home',firstName:'omar',lastName:'yaakoubi',tel:56990345,child:[340,339,338]},
+    {email:'a@gm.com',id:5,age:30,sexe:'home',firstName:'hama',lastName:'bouali',tel:56990345,child:[341,344,343]},
+    {email:'a@gm.com',id:6,age:30,sexe:'home',firstName:'jobran',lastName:'arfi',tel:56990345,child:[347,346,345]},
+    {email:'a@gm.com',id:7,age:30,sexe:'home',firstName:'omar',lastName:'hamroun',tel:56990345,child:[348,349,350]},
+]
+
+export const child=[
+    {id:330,age:3,sexe:'home',firstName:'weal',lastName:'hajri'},
+    {id:331,age:2,sexe:'home',firstName:'loua',lastName:'fahmi'},
+    {id:332,age:3,sexe:'home',firstName:'lamjid',lastName:'amairi'},
+    {id:333,age:3,sexe:'home',firstName:'fathi',lastName:'yaakoubi'},
+    {id:334,age:3,sexe:'home',firstName:'radhi',lastName:'bouali'},
+    {id:335,age:3,sexe:'home',firstName:'moujib',lastName:'arfi'},
+    {id:337,age:3,sexe:'home',firstName:'youssef',lastName:'hamroun'},
+]
+
+export const prof=[
+    {email:'p1@gm.com',id:1111,age:30,sexe:'home',firstName:'p1',lastName:'p1 last',tel:56990345,child:[330,331,332]},
+    {email:'p2@gm.com',id:1112,age:30,sexe:'home',firstName:'p2',lastName:'p2 last',tel:56955345,child:[333,334,337]},
+]
